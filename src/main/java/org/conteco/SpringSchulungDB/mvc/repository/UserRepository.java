afa1b519a3fb1ac0f15c5f649ed195b6c580b376
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-05-10 15:04:56 +0200

    Query added

--- src/main/java/org/conteco/SpringSchulungDB/mvc/repository/UserRepository.java
+++ src/main/java/org/conteco/SpringSchulungDB/mvc/repository/UserRepository.java
@@ -2,9 +2,18 @@ package org.conteco.SpringSchulungDB.mvc.repository;
 
 import org.conteco.SpringSchulungDB.mvc.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByName(String name);
+
+    @Query("select u from User u where u.name like concat(?1, '%')")
+    List<User> findByNameStartsWith(String name);
+
+
+
+
 }
