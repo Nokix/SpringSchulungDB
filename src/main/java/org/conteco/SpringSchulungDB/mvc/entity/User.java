8c3ecc3c55b1ac31cb20a1122c00d68f5b16130a
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-05-10 13:18:33 +0200

    User Entity hinzugefügt

--- src/main/java/org/conteco/SpringSchulungDB/mvc/entity/User.java
+++ src/main/java/org/conteco/SpringSchulungDB/mvc/entity/User.java
@@ -2,13 +2,22 @@ package org.conteco.SpringSchulungDB.mvc.entity;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+@Table(name = "Benutzer")
 public class User {
+    @Id
     private Long id;
+
     private String name;
 
     public User(String name) {
