bed2d2fdb9146d06df8752cb3f019f6c4179b251
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-05-10 11:26:54 +0200

    Kafka Producer eingebaut

--- src/main/java/org/conteco/SpringSchulungDB/kafka/KafkaController.java
+++ src/main/java/org/conteco/SpringSchulungDB/kafka/KafkaController.java
@@ -1,14 +1,23 @@
 package org.conteco.SpringSchulungDB.kafka;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Profile("kafka")
 public class KafkaController {
+    @Autowired
+    KafkaTemplate<String, String> template;
 
-
+    @GetMapping("/kafka/{message}")
+    public void postToKafka(@PathVariable String message) {
+        template.send("topic1", message);
+    }
 
     @KafkaListener(id = "ViktorListener", topics = "topic1")
     public void listen(String value) {
