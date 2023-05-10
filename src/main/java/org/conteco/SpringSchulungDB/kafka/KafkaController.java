178e4124dd6d38aba28a3cc2e10785c98036e972
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-05-10 11:17:04 +0200

    KafkaListener

--- src/main/java/org/conteco/SpringSchulungDB/kafka/KafkaController.java
+++ src/main/java/org/conteco/SpringSchulungDB/kafka/KafkaController.java
@@ -1,10 +1,17 @@
 package org.conteco.SpringSchulungDB.kafka;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Profile("kafka")
 public class KafkaController {
 
+
+
+    @KafkaListener(id = "ViktorListener", topics = "topic1")
+    public void listen(String value) {
+        System.out.println(value);
+    }
 }
