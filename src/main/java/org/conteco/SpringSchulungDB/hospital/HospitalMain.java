185e439ca9ec24e29ccc65ec335b6f8fa1aa01d6
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-05-10 11:13:02 +0200

    Kafka vorbereitet: application-kafka.propoerties

--- src/main/java/org/conteco/SpringSchulungDB/hospital/HospitalMain.java
+++ src/main/java/org/conteco/SpringSchulungDB/hospital/HospitalMain.java
@@ -3,7 +3,7 @@ package org.conteco.SpringSchulungDB.hospital;
 import org.springframework.context.annotation.*;
 
 @ComponentScan
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:application-kafka.properties")
 public class HospitalMain {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
---
+++ src/main/java/org/conteco/SpringSchulungDB/kafka/KafkaController.java
@@ -0,0 +1,10 @@
+package org.conteco.SpringSchulungDB.kafka;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Profile("kafka")
+public class KafkaController {
+
+}
